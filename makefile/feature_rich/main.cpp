8fb78e627536bc92cd02684ac6edf271f356547e
Kvoss <kvoss_git@freenet.de>
2023-10-30 17:58:58 +0100

    update
    Signed-off-by: Kvoss <kvoss_git@freenet.de>

--- makefile/feature_rich/main.cpp
+++ makefile/feature_rich/main.cpp
@@ -13,7 +13,7 @@ int main()
 
 void write_makefile_featurerich()
 {
-  char filename[] = "makefile";
+  char filename[] = "makefile_test";
   FILE *fptr = NULL;
   char info[] = "# see "
                 "https://github.com/gwu-cs-os/evening_os_hour/blob/master/f19/"
@@ -22,14 +22,17 @@ void write_makefile_featurerich()
   char codedirs[] = "CODEDIRS=. ./src # source files";
   char incdirs[] = "INCDIRS=. ./header # can be list of headers";
   char compiler[] = "CXX=g++";
-  char opts[] = "OPT=-O0";
+  char opts_debug[] = "OPT_DEBUG=-O0";
+  char opts_release[] = "OPT_RELEASE=-O2";
   char comment_header_dependency[] =
       "# generate files that encode make rules for the .h dependencies";
   char depflags[] = "DEPFLAGS=-MP -MD";
   char comment_autoinclude_incdirs[] = "# automatically add the -I onto each include directory";
-  char cflags[] = "CXXFLAGS=-Wall -Wextra -g $(foreach D,$(INCDIRS),-I$(D)) "
-                  "$(OPT) $(DEPFLAGS)";
-  char lflags[] = "LDFLAGS= # math: -lm | ncurses: -lncurses -ltinfo -lmenu";
+  char cxxflags_debug[] = "CXXFLAGS_DEBUG=-Wall -Wextra -g $(foreach D,$(INCDIRS),-I$(D)) "
+                          "$(OPT_DEBUG) $(DEPFLAGS)";
+  char cxxflags_release[] =
+      "CXXFLAGS_RELEASE=-Wall -Wextra $(foreach D,$(INCDIRS),-I$(D)) $(OPT_RELEASE) $(DEPFLAGS)";
+  char ldflags[] = "LDFLAGS= # math: -lm | ncurses: -lncurses -ltinfo -lmenu";
   char comment_regular_expression_completion_c_sources[] =
       "# for-style iteration (foreach) and regular expression completions "
       "(wildcard)";
@@ -42,7 +45,8 @@ void write_makefile_featurerich()
   char build_binaries_recipe[] = "$(CXX) -o $@ $^ $(LDFLAGS)";
   char comment_c_dependency[] = "# only want the .c file dependency here, thus $< instead of $^.";
   char build_objs[] = "%.o:%.cpp";
-  char build_objs_recipe[] = "$(CXX) $(CXXFLAGS) -c -o $@ $<";
+  char build_objs_recipe_debug[] = "$(CXX) $(CXXFLAGS_DEBUG) -c -o $@ $<";
+  // char build_objs_recipe_release[] = "$(CXX) $(CXXFLAGS_RELEASE) -c -o $@ $<";
   char cleanup[] = "clean:";
   char cleanup_recipe[] = "rm -rf $(BINARY) $(OBJECTS) $(DEPFILES)";
   char comment_shell_cmds[] = "# shell commands are a set of keystrokes away";
@@ -76,12 +80,14 @@ void write_makefile_featurerich()
     fprintf(fptr, "%s\n", codedirs);
     fprintf(fptr, "%s\n\n", incdirs);
     fprintf(fptr, "%s\n", compiler);
-    fprintf(fptr, "%s\n", opts);
+    fprintf(fptr, "%s\n", opts_debug);
+    fprintf(fptr, "%s\n", opts_release);
     fprintf(fptr, "%s\n", comment_header_dependency);
     fprintf(fptr, "%s\n", depflags);
     fprintf(fptr, "%s\n", comment_autoinclude_incdirs);
-    fprintf(fptr, "%s\n", cflags);
-    fprintf(fptr, "%s\n\n", lflags);
+    fprintf(fptr, "%s\n", cxxflags_debug);
+    fprintf(fptr, "%s\n", cxxflags_release);
+    fprintf(fptr, "%s\n\n", ldflags);
     fprintf(fptr, "%s\n", comment_regular_expression_completion_c_sources);
     fprintf(fptr, "%s\n", cfiles);
     fprintf(fptr, "%s\n", comment_regex_replacement);
@@ -92,7 +98,8 @@ void write_makefile_featurerich()
     fprintf(fptr, "\t%s\n\n", build_binaries_recipe);
     fprintf(fptr, "%s\n", comment_c_dependency);
     fprintf(fptr, "%s\n", build_objs);
-    fprintf(fptr, "\t%s\n\n", build_objs_recipe);
+    fprintf(fptr, "\t%s\n\n", build_objs_recipe_debug);
+    // fprintf(fptr, "\t%s\n\n", build_objs_recipe);
     fprintf(fptr, "%s\n", cleanup);
     fprintf(fptr, "\t%s\n\n", cleanup_recipe);
     fprintf(fptr, "%s\n", comment_shell_cmds);
